272451be1ef57da65f82d088a9ec5ac59e0ba461
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-02-24 00:30:48 +0600

    Services page is added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Benefit from '../Benefit/Benefit';
+import Services from '../Services/Services';
 import Subscribe from '../Subscribe/Subscribe';
 
 const Home = () => {
     return (
         <div>
+            <Services></Services>
             <Benefit></Benefit>
             <Subscribe></Subscribe>
         </div>
