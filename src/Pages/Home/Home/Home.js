6595c2dfa9b613ad273b7371122028b9331ea3eb
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-02-24 00:10:34 +0600

    Benefit page is added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Benefit from '../Benefit/Benefit';
 import Subscribe from '../Subscribe/Subscribe';
 
 const Home = () => {
     return (
         <div>
+            <Benefit></Benefit>
             <Subscribe></Subscribe>
         </div>
     );
