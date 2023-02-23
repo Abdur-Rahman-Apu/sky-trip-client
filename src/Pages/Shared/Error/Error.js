b428ea7411752e4325055e20d4812b631c6fdcd7
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-02-23 22:53:47 +0600

    Error page is created

--- src/Pages/Shared/Error/Error.js
+++ src/Pages/Shared/Error/Error.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import Footer from '../Footer/Footer';
+import Navbar from '../Navbar/Navbar';
+import Lottie from "lottie-react";
+import Err from '../../../assets/error.json';
 
 const Error = () => {
     return (
         <div>
-            Error
+            <Navbar></Navbar>
+            <div className='w-96 mx-auto'>
+                <Lottie animationData={Err} loop={true} />
+            </div>
+            <Footer></Footer>
         </div>
     );
 };
