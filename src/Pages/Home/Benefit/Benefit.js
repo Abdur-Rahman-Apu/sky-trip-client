9024c7179d2c5291e05be7df77d269a77165846d
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-02-24 00:19:05 +0600

    Benefit responsive issue is solved

--- src/Pages/Home/Benefit/Benefit.js
+++ src/Pages/Home/Benefit/Benefit.js
@@ -5,23 +5,23 @@ import './Benefit.css'
 
 const Benefit = () => {
     return (
-        <div className='flex flex-col md:flex-row justify-center items-center my-24 px-14 h-[400px]'>
+        <div className='flex flex-col md:flex-row justify-center items-center my-24 px-14 h-[900px] lg:h-[400px]'>
             <div className='basis-1/2'>
                 <h1 className='mb-5 text-3xl font-bold'>Unaccompanied Minor Lounge</h1>
                 <div className='flex flex-wrap'>
-                    <div className='basis-1/2 mb-4 '>
+                    <div className='basis-full md:basis-1/2 mb-4 '>
                         <h3 className='text-lg font-bold'>Help through the airport</h3>
                         <p className='pr-4 text-sm'>You can also call airlines from your phone and book a flight ticket to one of your favorite destinations.</p>
                     </div>
-                    <div className='basis-1/2 mb-4'>
+                    <div className='basis-full md:basis-1/2 mb-4 '>
                         <h3 className='text-lg font-bold'>Priority Boarding</h3>
                         <p className='pr-4 text-sm'>You can also call airlines from your phone and book a flight ticket to one of your favorite destinations.</p>
                     </div>
-                    <div className='basis-1/2'>
+                    <div className='basis-full md:basis-1/2 mb-4 '>
                         <h3 className='text-lg font-bold'>Care on the flight</h3>
                         <p className='pr-4 text-sm'>You can also call airlines from your phone and book a flight ticket to one of your favorite destinations.</p>
                     </div>
-                    <div className='basis-1/2'>
+                    <div className='basis-full md:basis-1/2 mb-4 '>
                         <h3 className='text-lg font-bold'>Chouffeur-drive service</h3>
                         <p className='pr-4 text-sm'>You can also call airlines from your phone and book a flight ticket to one of your favorite destinations.</p>
                     </div>
@@ -31,7 +31,7 @@ const Benefit = () => {
                 <div className='rounded-full w-52 h-[300px]'>
                     <img className=' h-full object-cover' src={BenefitImg1} alt="benefit-img" />
                 </div>
-                <div className='rounded-full w-40 h-[250px] absolute top-32 right-24'>
+                <div className='rounded-full hidden sm:block w-40 h-[250px] absolute top-32 right-24'>
                     <img className=' h-full object-cover' src={BenefitImg2} alt="benefit-img" />
                 </div>
             </div>
