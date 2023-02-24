d645e7536203b06c5e6f73537743620ea91712e8
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-02-24 14:34:39 +0600

    Search issues are solved

--- src/Pages/Home/Search/Search.js
+++ src/Pages/Home/Search/Search.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Search = () => {
     return (
-        <div className='w-96 mx-auto mt-28'>
+        <div className='w-[50vw] mx-auto mt-28'>
             <h1 className='text-2xl font-bold text-center mb-8'>Find your desired flight</h1>
-            <form className="flex items-center">
+            <form className="flex flex-col md:flex-row items-center">
                 <label for="voice-search" className="sr-only">Search</label>
                 <div className="relative w-full">
                     <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
@@ -15,7 +15,7 @@ const Search = () => {
                         <svg aria-hidden="true" className="w-4 h-4 text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7 4a3 3 0 016 0v4a3 3 0 11-6 0V4zm4 10.93A7.001 7.001 0 0017 8a1 1 0 10-2 0A5 5 0 015 8a1 1 0 00-2 0 7.001 7.001 0 006 6.93V17H6a1 1 0 100 2h8a1 1 0 100-2h-3v-2.07z" clipRule="evenodd"></path></svg>
                     </button>
                 </div>
-                <button type="submit" className="inline-flex items-center py-2.5 px-3 ml-2 text-sm font-medium text-white bg-deepViolet rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <button type="submit" className="inline-flex items-center py-2.5 mt-10 md:mt-0 px-3 ml-2 text-sm font-medium text-white bg-deepViolet rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     <svg aria-hidden="true" className="w-5 h-5 mr-2 -ml-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>Search
                 </button>
             </form>
