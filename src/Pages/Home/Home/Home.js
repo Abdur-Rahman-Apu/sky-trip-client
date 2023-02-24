70cf355a1813e73a10cd028692e9809a5cfbded9
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-02-24 10:35:28 +0600

    Banner is added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Facilities from '../../Facilities/Facilities';
+import Banner from '../Banner/Banner';
 import Benefit from '../Benefit/Benefit';
 import Services from '../Services/Services';
 import Subscribe from '../Subscribe/Subscribe';
@@ -7,6 +8,7 @@ import Subscribe from '../Subscribe/Subscribe';
 const Home = () => {
     return (
         <div>
+            <Banner></Banner>
             <Services></Services>
             <Facilities></Facilities>
             <Benefit></Benefit>
