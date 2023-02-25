9b7b27904e2a57be62eea4302fce004be4bd35e8
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-02-25 21:50:43 +0600

    Top traveler page dark mode finished

--- src/Pages/Home/TopTravelers/TopTravelers.js
+++ src/Pages/Home/TopTravelers/TopTravelers.js
@@ -3,7 +3,7 @@ import React from 'react';
 const TopTravelers = () => {
     return (
         <div className='my-10'>
-            <h1 className='text-2xl font-bold text-center'>Top travelers of this month!</h1>
+            <h1 className='text-2xl font-bold text-center dark:text-white'>Top travelers of this month!</h1>
         </div>
     );
 };
