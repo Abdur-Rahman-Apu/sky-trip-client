763fc280303251d0e172a3e2edbd8107b6f17bd1
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-02-25 19:42:07 +0600

    Banner responsive is done

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -4,15 +4,15 @@ import Airplane from '../../../assets/airplane.png'
 
 const Banner = () => {
     return (
-        <div>
-            <h1 className='text-4xl font-semibold text-center my-10'>Create Ever-lasting <br /> Memories With Us</h1>
+        <div className='pt-5'>
+            <h1 className='text-xl md:text-4xl font-bold text-center my-10'>Create Ever-lasting <br /> Memories With Us</h1>
 
             <div className='relative'>
                 <video autoPlay muted loop className=' mx-auto rounded-full md:h-[300px] '>
                     <source src={BannerVideo} type="video/mp4"></source>
                 </video>
 
-                <img className='absolute -top-8 md:-top-10 lg:left-80 h-[290px] md:h-[390px]' src={Airplane} alt="airplaneImg" />
+                <img className='absolute -top-8 md:-top-10 lg:left-80 h-[250px] md:h-[390px]' src={Airplane} alt="airplaneImg" />
             </div>
         </div>
     );
