6da9f947e863e1342df3f51853d7baa76f7a7dbf
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-02-27 21:12:05 +0600

    Index.js page is clean

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const queryClient = new QueryClient()
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
