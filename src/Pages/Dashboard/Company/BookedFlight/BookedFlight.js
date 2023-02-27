da39b2a51e9ddd0bec9d6172fb933ec8a0e5cb67
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-02-27 07:17:01 +0600

    Display comapany paid item

--- src/Pages/Dashboard/Company/BookedFlight/BookedFlight.js
+++ src/Pages/Dashboard/Company/BookedFlight/BookedFlight.js
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../../../Context/AuthProvider';
+import DisplayCompanyPaidInfo from './DisplayCompanyPaidInfo';
 
 const BookedFlight = () => {
 
@@ -15,10 +16,35 @@ const BookedFlight = () => {
             })
     }, [user?.email])
 
+    console.log("Company paid info", companyPaidInfo);
 
     return (
         <div>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>Buyer Email</th>
+                            <th>Transaction Id</th>
+                            <th>From</th>
+                            <th>Destination</th>
+                            <th>Flight id</th>
+                            <th>time</th>
+                            <th>Flight Date</th>
+                            <th>seat</th>
+                            <th>Price</th>
+                        </tr>
+                    </thead>
+                    <tbody>
 
+                        {
+                            companyPaidInfo.map(item => <DisplayCompanyPaidInfo key={item._id} item={item}></DisplayCompanyPaidInfo>)
+                        }
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
---
+++ src/Pages/Dashboard/Company/BookedFlight/DisplayCompanyPaidInfo.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const DisplayCompanyPaidInfo = ({ item }) => {
+    console.log(item);
+    return (
+        <tr>
+            <td>{item?.buyerEmail}</td>
+            <td>{item?.transactionId}</td>
+            <td>{item?.flightInfo?.from}</td>
+            <td>{item?.flightInfo?.destination}</td>
+            <td>{item?.flightInfo?._id}</td>
+            <td>{item?.flightInfo?.time}</td>
+            <td>{item?.flightInfo?.flightDate}</td>
+            <td>{item?.bookInfo?.seat}</td>
+            <td>{item?.price}</td>
+
+        </tr>
+    );
+};
+
+export default DisplayCompanyPaidInfo;
