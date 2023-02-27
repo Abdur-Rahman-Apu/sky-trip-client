b49352fd1fff1ef8048d78b65caddba96d1d647f
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-02-27 20:55:57 +0600

    Admin page is clean

--- src/Pages/Dashboard/Admin/AllUser/DisplayUser.js
+++ src/Pages/Dashboard/Admin/AllUser/DisplayUser.js
@@ -1,17 +1,16 @@
 import React from 'react';
 
 const DisplayUser = ({ user, handleDelete }) => {
+
     const { _id, name, image, email } = user;
+
     return (
         <tr>
             <td>
                 <h1 className='font-bold'>{name}</h1>
             </td>
             <td>
-
                 <img className='w-36' src={image} alt="img" />
-
-
             </td>
 
             <td>
